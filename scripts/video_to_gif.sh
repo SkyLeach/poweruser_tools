41755d31ddd09afac56a3befc7607387a4bc4c84
magregor <magregor@cisco.com>
2017-10-22 22:41:31 -0400

    Improved option handling.

--- scripts/video_to_gif.sh
+++ scripts/video_to_gif.sh
@@ -8,37 +8,95 @@ me=${0##*/}
 vid="${@: -1}"
 if  [ ! -f "${vid}" ]
 then
-    echo "Usage: [options] $me <vidfile> [scale-width]" >&2
-    echo "Options:"
-    echo "    -ss=###        : # of seconds to skip ahead in vidfile"
-    echo "    -t=###         : Length, in seconds, of output gif"
-    echo "    -w|--width=### : Rescale (lanczos) output retaining aspect ration to a maximum width of ###"
-    echo "    -O=<outfile>   : The output gif will have the name supplied."
-    exit 2
+    show_help # display options
 fi
 
+die() {
+    printf '%s\n' "$1" >&2
+    exit 1
+}
+
+show_help() {
+    echo "Usage: [options] $me <vidfile> [scale-width]" >&2
+    echo "Options:"
+    echo "    -ss ###        : # of seconds to skip ahead in vidfile"
+    echo "    -t ###         : Length, in seconds, of output gif"
+    echo "    -w|--width ### : Rescale (lanczos) output retaining aspect ration to a maximum width of ###"
+    echo "    -O <outfile>   : The output gif will have the name supplied."
+    exit 2
+}
 # parse allowed arguments
-for i in "$@"
-do
-    case $i in
+# for i in "$@"
+# do
+#    case $i in
+while :; do
+    case $1 in
+        -ss)
+            if [ "$2" ]; then
+                SS="-ss $2"
+                shift
+            else
+                die 'ERROR: "-ss" requires a non-empty option argument'
+            fi
+        ;;
         -ss=*)
-        SS="-ss ${i#*=}"
-        echo ${SS}
+            SS="-ss ${1#*=}"
+            echo ${SS}
+        ;;
+        -ss=)
+            die 'ERROR: "-ss" requires a non-empty option argument'
+        ;;
+        -t)
+            if [ "$2" ]; then
+                T="-t $2"
+                shift
+            else
+                die 'ERROR: "-t" requires a non-empty option argument'
+            fi
         ;;
         -t=*)
-        T="-t ${i#*=}"
-        echo ${T}
+            T="-t ${1#*=}"
+            echo ${T}
+        ;;
+        -t=)
+            die 'ERROR: "-t" requires a non-empty option argument'
+        ;;
+        -O)
+            if [ "$2" ]; then
+                OUT="$2"
+                shift
+            else
+                die 'ERROR: "-O" requires a non-empty option argument'
+            fi
         ;;
         -O=*)
-        OUT="${i#*=}"
-        echo ${OUT}
+            OUT="${1#*=}"
+            echo ${OUT}
+        ;;
+        -O=)
+            die 'ERROR: "-O" requires a non-empty option argument'
+        ;;
+        -w|--width)
+            if [ "$2" ]; then
+                WIDTH=",scale=$2"
+                shift
+            else
+                die 'ERROR: "-w|--width" requires a non-empty option argument'
+            fi
         ;;
         -w=*|--width=*)
-        WIDTH=",scale=${i#*=}"
+            WIDTH=",scale=${1#*=}"
         ;;
-        *)
+        -w=|--width=)
+            die 'ERROR: "-w|--width" requires a non-empty option argument'
         ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *) # Default case: No more options, so break out of the loop.
+            break
     esac
+    shift
 done
 
 # if output not set, set it
