1a942e9b6311c2d85473b8bd1cf86835f41c0e22
magregor <magregor@cisco.com>
2018-04-12 13:13:42 -0400

    Missed a small change in the previosu commit

--- scripts/restore_vim_bundles.sh
+++ scripts/restore_vim_bundles.sh
@@ -1,58 +1,103 @@
 #!/usr/bin/env bash
 # vim: ts=4 sw=4 sts=4 et nofoldenable
+# parse options
+# tmp=$(mktemp /tmp/palette.png)
+# trap "rm -f $tmp" 0
+me=${0##*/}
+if [ $(pushd $(dirname "${0}" > /dev/null 2>&1 ) > /dev/null 2>&1 ) ]; then
+    mepath=$(pwd)
+    popd > /dev/null
+else
+    mepath=$(dirname "${0}")
+fi
+
+die() {
+    printf '%s\n' "$1" >&2
+    exit 1
+}
+
+show_help() {
+    echo "Usage: [options] $me" >&2
+    echo "    uses git clone to restore your bundles in your "+'$VIMHOME'+" or "+'$NVIMHOME'
+    echo "Options:"
+    echo "    -h|--help:    print this help message"
+    echo "    -n|--nvim:    only operate on "+'${NVIMHOME}'
+    echo "    -v|--vim:     only operate on "+'${VIMHOME}'
+    echo "    -O <outfile>: Reads and outputs the list of remotes named 'origin'."
+    exit 2
+}
+# parse allowed arguments
+# for i in "$@"
+# do
+#    case $i in
+while :; do
+    case $1 in
+        -h|--help)
+            show_help
+            die 'ERROR: "-w|--width" requires a non-empty option argument'
+        ;;
+        -n)
+            onlyon="${NVIMHOME}/bundle"
+        ;;
+        -v)
+            onlyon="${VIMHOME}/bundle"
+        ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *) # Default case: No more options, so break out of the loop.
+            break
+    esac
+    shift
+done
 if [ -z "${VIMHOME}" ] && [ -z "${NVIMHOME}" ]; then 
-    echo "Cowardly refusing to do anything because you haven't set VIMHOME." 
-    exit -1
+    die "Cowardly refusing to do anything because you haven't set *any* VIMHOME or NVIMHOME." 
+elif [ -L "${VIMHOME}/bundle" ] || [ -L "${NVIMHOME}/bundle" ]; then
+    # see if they point to each other
+    vimblink="$(readlink -n "${VIMHOME}/bundle")"
+    nvimblink="$(readlink -n "${NVIMHOME}/bundle")"
+    if [ -z "${vimblink}" ]; then
+        # vim bundle dir is no link
+        left="${VIMHOME}/bundle"
+        right="${nvimblink}"
+    elif [ -z "${nvimblink}" ]; then
+        # nvim bundle dir is no link
+        left="${vimblink}"
+        right="${NVIMHOME}/bundle"
+    else
+        # both are links
+        left="${vimblink}"
+        right="${nvimblink}"
+    fi
+    if [ "${left}" == "${right}" ]; then
+        # doesn't matter, go with right
+        onlyon="${right}"
+        workon=("${onlyon}")
+    fi
+elif [ ! -z "${onlyon}" ]; then
+    # if onlyon is set then we're good
+    workon=("${onlyon}")
+elif [ -z "${VIMHOME}" ]; then
+    # make workon point to nvimhome/bundle
+    workon=("${NVIMHOME}/bundle")
+else
+    # make workon point to vimhome/bundle
+    workon=("${VIMHOME}/bundle")
 fi
 cwd_save=$(pwd)
-vidr=''
-[[ -z "${NVIMHOME}" ]] && vdir="${VIMHOME}" || vdir="${NVIMHOME}"
-cd "${vdir}"
-cd ./bundle || ( mkdir ./bundle && cd ./bundle )
-[[ "$?" -ne "0" ]] && ( echo "Unable to make or find bundle path." && exit -1 )
-echo got here at least
-echo "$(pwd)"
-git clone https://github.com/cohama/agit.vim
-git clone https://github.com/w0rp/ale.git
-git clone https://github.com/vim-scripts/Align
-git clone https://github.com/prabirshrestha/async.vim
-git clone https://github.com/prabirshrestha/asyncomplete-lsp.vim
-git clone https://github.com/prabirshrestha/asyncomplete.vim
-git clone https://github.com/jlanzarotta/bufexplorer
-git clone https://github.com/vim-scripts/dbext.vim
-git clone https://github.com/zchee/deoplete-jedi
-git clone https://github.com/Shougo/deoplete.nvim
-git clone https://github.com/Yggdroot/LeaderF
-git clone https://github.com/mattn/livestyle-vim
-git clone https://github.com/JalaiAmitahl/maven-compiler.vim
-git clone http://github.com/yegappan/mru
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/rhysd/nyaovim-markdown-preview
-git clone https://github.com/rhysd/nyaovim-mini-browser
-git clone https://github.com/rhysd/nyaovim-popup-tooltip
-git clone https://github.com/aklt/plantuml-syntax
-git clone https://github.com/Rykka/riv.vim
-git clone https://github.com/vim-scripts/SQLUtilities
-git clone https://github.com/ervandew/supertab
-git clone https://github.com/vim-airline/vim-airline
-git clone https://github.com/flazz/vim-colorschemes
-git clone https://github.com/jtratner/vim-flavored-markdown
-git clone git://github.com/tpope/vim-fugitive.git
-git clone https://github.com/raghur/vim-ghost
-git clone https://github.com/tfnico/vim-gradle
-git clone https://github.com/pangloss/vim-javascript
-git clone https://github.com/lepture/vim-jinja
-git clone https://github.com/elzr/vim-json
-git clone https://github.com/dbakker/vim-lint.git
-git clone https://github.com/tetsuo13/Vim-log4j
-git clone https://github.com/prabirshrestha/vim-lsp
-git clone https://github.com/tpope/vim-markdown
-git clone https://github.com/xolox/vim-misc
-git clone https://github.com/sloria/vim-ped
-git clone https://github.com/heavenshell/vim-pydocstring
-git clone https://github.com/xolox/vim-session
-git clone git://github.com/tpope/vim-surround.git
-git clone https://github.com/tmux-plugins/vim-tmux-focus-events
-git clone http://github.com/rkitover/vimpager
+# die "${!workon[*]}"
+for target in "${workon[@]}"; do
+    cd "${target}"
+    while read -r origin; do
+        reponame=$(basename -s .git -- "${origin}")
+        if [ -d "${reponame}" ]; then
+            echo "${reponame} exists, skipping..."
+            continue
+        else
+            echo "${reponame} missing, clonging..."
+        fi
+    done < "${mepath}/../config_files/pathogen_bundles.txt"
+    cd ..
+done
 cd "${cwd_save}"
 exit 0
