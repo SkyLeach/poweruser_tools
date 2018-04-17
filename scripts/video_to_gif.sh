44ef32a5e50c0e477093768507d554896396b569
magregor <magregor@cisco.com>
2018-04-17 02:55:28 -0400

    fix major issues with this script, but I want to rewrite it

--- scripts/video_to_gif.sh
+++ scripts/video_to_gif.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 # an ffmpeg wrapper script to extract a color pallet and then a video to an
 # animated gif.  Optional user arguments
-
-tmp=$(mktemp /tmp/palette.png)
-trap "rm -f $tmp" 0
-me=${0##*/}
-vid="${@: -1}"
-if  [ ! -f "${vid}" ]
-then
-    show_help # display options
-fi
-
 die() {
     printf '%s\n' "$1" >&2
     exit 1
 }
-
 show_help() {
+    echo "${1}"
     echo "Usage: [options] $me <vidfile> [scale-width]" >&2
     echo "Options:"
     echo "    -ss ###        : # of seconds to skip ahead in vidfile"
@@ -25,89 +15,106 @@ show_help() {
     echo "    -O <outfile>   : The output gif will have the name supplied."
     exit 2
 }
-# parse allowed arguments
-# for i in "$@"
-# do
-#    case $i in
-while :; do
-    case $1 in
+# initial config, first argument
+me="${0##*/}"
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+    case $key in
         -ss)
             if [ "$2" ]; then
                 SS="-ss $2"
                 shift
             else
-                die 'ERROR: "-ss" requires a non-empty option argument'
+                show_help 'ERROR: "-ss" requires a non-empty option argument'
             fi
+        ;;
+        -ss=)
+            show_help 'ERROR: "-ss" requires a non-empty option argument'
         ;;
         -ss=*)
             SS="-ss ${1#*=}"
-            echo ${SS}
-        ;;
-        -ss=)
-            die 'ERROR: "-ss" requires a non-empty option argument'
+            echo "${SS}"
         ;;
         -t)
             if [ "$2" ]; then
                 T="-t $2"
                 shift
             else
-                die 'ERROR: "-t" requires a non-empty option argument'
+                show_help 'ERROR: "-t" requires a non-empty option argument'
             fi
+        ;;
+        -t=)
+            show_help 'ERROR: "-t" requires a non-empty option argument'
         ;;
         -t=*)
             T="-t ${1#*=}"
-            echo ${T}
-        ;;
-        -t=)
-            die 'ERROR: "-t" requires a non-empty option argument'
+            echo "${T}"
         ;;
         -O)
             if [ "$2" ]; then
                 OUT="$2"
                 shift
             else
-                die 'ERROR: "-O" requires a non-empty option argument'
+                show_help 'ERROR: "-O" requires a non-empty option argument'
             fi
+        ;;
+        -O=)
+            show_help 'ERROR: "-O" requires a non-empty option argument'
         ;;
         -O=*)
             OUT="${1#*=}"
-            echo ${OUT}
-        ;;
-        -O=)
-            die 'ERROR: "-O" requires a non-empty option argument'
+            echo "${OUT}"
         ;;
         -w|--width)
             if [ "$2" ]; then
                 WIDTH=",scale=$2"
                 shift
             else
-                die 'ERROR: "-w|--width" requires a non-empty option argument'
+                show_help 'ERROR: "-w|--width" requires a non-empty option argument'
             fi
+        ;;
+        -w=|--width=)
+            show_help 'ERROR: "-w|--width" requires a non-empty option argument'
         ;;
         -w=*|--width=*)
             WIDTH=",scale=${1#*=}"
         ;;
-        -w=|--width=)
-            die 'ERROR: "-w|--width" requires a non-empty option argument'
+        -h|--help)
+            show_help
+            exit 0
         ;;
         -?*)
             printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
             ;;
-        *) # Default case: No more options, so break out of the loop.
-            break
+        *)  # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
     esac
     shift
 done
-
+set -- "${POSITIONAL[@]}" # restore positional parameters
+# vid="${@: -1}"
+vid="${1}"
+if [ -z "${vid}" ] || [ ! -f "${vid}" ]; then
+    show_help "You must provide a video file to operate on." # display options
+fi
+tmp=$(mktemp /tmp/palette.png)
+trap 'rm -f $tmp' 0
+echo "vid = ${vid}"
+echo "out = ${OUT}"
+# show_help "You should have stopped already."
 # if output not set, set it
-if [ -z $OUT ]
+if [ -z "$OUT" ]
 then
-   OUT=$(basename ${vid})
+   OUT=$(basename "${vid}")
    OUT="${OUT%.*}.gif"
 fi
 
-ffmpeg -y ${SS} ${T} -i "${vid}" \
-    -vf fps=10${WIDTH}:-1:flags=lanczos,palettegen "${tmp}"
+ffmpeg -y "${SS}" "${T}" -i "${vid}" \
+    -vf "fps=10${WIDTH}:-1:flags=lanczos,palettegen" "${tmp}"
 
-ffmpeg ${SS} ${T} -i "${vid}" -i "${tmp}" -filter_complex \
-    "fps=10${WIDTH}:-1:flags=lanczos[x];[x][1:v]paletteuse" ${OUT}
+ffmpeg "${SS}" "${T}" -i "${vid}" -i "${tmp}" -filter_complex \
+    "fps=10${WIDTH}:-1:flags=lanczos[x];[x][1:v]paletteuse" "${OUT}"
