dd96ba7499a946af793b324ca0c6a51c1c3fc79e
Matt Gregory <skyleach@gmail.com>
2025-11-26 17:23:16 -0500

    feat: migrate to selenium with Firefox profile for PyPI scraping
    Replace requests-html with selenium webdriver to use Firefox with default
    profile. This bypasses JavaScript rendering issues and allows using existing
    browser sessions. The script now reliably scrapes PyPI search results with
    proper JavaScript execution support.

--- scripts/beautifulsoup4_sample.py
+++ scripts/beautifulsoup4_sample.py
@@ -1,14 +1,56 @@
-import bs4, requests
+import os
+import time
+import bs4
+from selenium import webdriver
+#  from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
-query = "soup"
 
-response = requests.get("https://pypi.org/search/?", {"q": query})
-html = response.content
-bs = bs4.BeautifulSoup(html)
-pkg_els = list(bs.select(".package-snippet"))
+def main():
+    query = "soup"
 
-pfx = "package-snippet__"
-info_list = [dict((fld, pkg_el.select(f".{pfx}{fld}")[0].text.strip().split("\n\n")[0]) for fld in ("title", "name", "version", "released", "description")) for pkg_el in pkg_els]
-info = dict((i["name"],i) for i in info_list)
+    # Configure Firefox options to use your default profile
+    firefox_options = Options()
+    firefox_options.add_argument("--profile")
+    firefox_options.add_argument(
+            os.path.join(os.environ['APPDATA'],
+            r"\Mozilla\Firefox\Profiles\5wjx7aqn.default-release"))
+    #  firefox_options.add_argument(
+    #          os.path.join(*["Users", "mattg", "AppData", "Local",
+    #                         "Mozilla", "Firefox", "Profiles",
+    #                         "5wjx7aqn.default-release"]))
 
-print(info)
+    # You can also let Firefox use the default profile automatically
+    # firefox_options.add_argument("--profile")
+    # firefox_options.add_argument(r"C:\Users\%USERNAME%\AppData\Roaming\Mozilla\Firefox\Profiles\*.default")
+
+    # Initialize Firefox driver
+    driver = webdriver.Firefox(options=firefox_options)
+
+    try:
+        # Navigate to PyPI search
+        driver.get(f"https://pypi.org/search/?q={query}")
+
+        # Wait for JavaScript to load (adjust time as needed)
+        time.sleep(3)
+
+        # Get the page source after JavaScript execution
+        html = driver.page_source
+        bs = bs4.BeautifulSoup(html)
+        pkg_els = list(bs.select(".package-snippet"))
+
+        pfx = "package-snippet__"
+        info_list = [dict((fld, pkg_el.select(
+        f".{pfx}{fld}")[0].text.strip().split("\n\n")[0]) for fld in (
+            "title", "name", "version", "released", "description")
+                                            ) for pkg_el in pkg_els]
+        info = dict((i["name"], i) for i in info_list)
+
+        print(info)
+
+    finally:
+        driver.quit()
+
+# Run the function
+if __name__ == "__main__":
+    main()
